d7011a36c0cd64257982affdbb8efacbd91917a6
Arthur <44406493+4ortytwo@users.noreply.github.com>
2021-04-19 23:28:36 +0200

    update with-realm-web example
    Check for a stale token and refresh it instead of creating a new user every time it expires.

--- examples/with-realm-web/lib/RealmClient.js
+++ examples/with-realm-web/lib/RealmClient.js
@@ -11,7 +11,11 @@ const app = new App({
 export const generateAuthHeader = async () => {
   if (!app.currentUser) {
     // If no user is logged in, log in an anonymous user
-    await app.logIn(Credentials.anonymous())
+    await app.logIn(Credentials.anonymous());
+  } else {
+    // An already logged in user's access token might be stale. To guarantee that the token is
+    // valid, we refresh the user's custom data which also refreshes their access token.
+    await app.currentUser.refreshCustomData();
   }
   // Get a valid access token for the current user
   const { accessToken } = app.currentUser
